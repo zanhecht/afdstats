b0567032fd592d2babdba4069ed1166716700892
zanhecht <797985+zanhecht@users.noreply.github.com>
2025-01-15 01:18:32 -0500

    Update app.py

--- www/python/src/app.py
+++ www/python/src/app.py
@@ -44,17 +44,16 @@ def app(environ, start_response):
 	if path_info == '/':
 		path_info = '/index.html'
 	file_name = path_info.lstrip('/')
-	if not path_info.startswith('/static/'):
-		static_file_path = os.path.join(static_dir, file_name)
-		if os.path.exists(static_file_path):
-			with open(static_file_path, 'rb') as f:
-				content = f.read()
-			ext = os.path.splitext(path_info)[1]
-			content_type = CONTENT_TYPES.get(ext, 'application/octet-stream')
-			start_response('200 OK', [('Content-Type', content_type)])
-			return [content]
-		elif file_name == 'afdstats.py':
-			return main(environ, start_response)
+	static_file_path = os.path.join(static_dir, file_name)
+	if os.path.exists(static_file_path):
+		with open(static_file_path, 'rb') as f:
+			content = f.read()
+		ext = os.path.splitext(path_info)[1]
+		content_type = CONTENT_TYPES.get(ext, 'application/octet-stream')
+		start_response('200 OK', [('Content-Type', content_type)])
+		return [content]
+	elif file_name == 'afdstats.py':
+		return main(environ, start_response)
 	start_response('404 Not Found', [('Content-Type', 'text/plain')])
 	return [b"404 Not Found"]
 
